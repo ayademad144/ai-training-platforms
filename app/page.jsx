d8049b5c6c72e2f13139248c2aa95fa0b0614706
Ayad Emad <ayad.emad144@gmail.com>
2026-07-13 04:10:40 +0300

    Add Navbar component and update styles; remove unused files and dependencies

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavbarSimple } from './components/NavbarSimple'
+const page = () => {
+  return (
+    <div>
+      {/* <NavbarSimple/> */}
+      home
+    </div>
+  )
+}
+
+export default page
